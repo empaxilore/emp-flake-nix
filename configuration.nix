d552bc5dfe36c45d55b0c52111d73ffbef3fb22d
empaxilore <190309694+empaxilore@users.noreply.github.com>
2025-03-25 12:16:27 -0400

    removed firefox

--- configuration.nix
+++ configuration.nix
@@ -86,8 +86,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-    firefox	
+  #  wget	
     vivaldi
     neovim
     git
