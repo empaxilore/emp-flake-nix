7376515413424c732302b2fd27f45d5e41449372
empaxilore <190309694+empaxilore@users.noreply.github.com>
2025-05-13 03:15:45 -0400

    trying some new colors

--- homeModules/terminals/kitty.nix
+++ homeModules/terminals/kitty.nix
@@ -5,9 +5,39 @@
 		programs.kitty = {
   			enable = true;
  			settings = {
-    			background_opacity = "0.9";
-    			font_name = "FiraCode Nerd Font"; # Optional, for icons
-			size = 12;
-  			};		
+				background_opacity = "0.9";
+				font_name = "Departure Mono"; # Updated to Departure Mono
+				font_size = "12";
+
+				# Gruvbox Dark color scheme
+				background = "#282828"; # bg0
+				foreground = "#ebdbb2"; # fg1
+				cursor = "#ebdbb2"; # fg1
+				selection_background = "#689d6a"; # green
+				selection_foreground = "#1d2021"; # bg0_h
+
+				# Colors for text attributes
+				color0 = "#282828"; # black (bg0)
+				color1 = "#cc241d"; # red
+			        color2 = "#98971a"; # green
+			      	color3 = "#d79921"; # yellow
+			      	color4 = "#458588"; # blue
+			      	color5 = "#b16286"; # purple
+			      	color6 = "#689d6a"; # aqua
+			      	color7 = "#a89984"; # gray
+			      	color8 = "#928374"; # bright black (gray)
+			      	color9 = "#fb4934"; # bright red
+			      	color10 = "#b8bb26"; # bright green
+			      	color11 = "#fabd2f"; # bright yellow
+			      	color12 = "#83a598"; # bright blue
+			      	color13 = "#d3869b"; # bright purple
+			      	color14 = "#8ec07c"; # bright aqua
+			      	color15 = "#ebdbb2"; # bright white (fg1)
+
+			      	# Tab bar styling for Gruvbox Dark
+			      	active_tab_background = "#fabd2f";
+			      	inactive_tab_background = "#3c3836";
+			      	tab_bar_background = "#1d2021";  			
+		       };		
 		};
 	}	
